bc24d60efc53727d80499cb38120c9a5c7177266
Philasande Bhani <85154876+pbhaninaa@users.noreply.github.com>
2023-05-17 07:22:05 +0200

    Commiting the changes

--- React Native. Job finder/app/home.js
+++ React Native. Job finder/app/home.js
@@ -13,7 +13,7 @@ import {
 const Home = () => {
   const router = useRouter()
   const [searchTerm, setSearchTerm] = useState("");
-// fffffffxdfcd
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
       <Stack.Screen
