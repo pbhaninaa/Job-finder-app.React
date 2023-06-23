4c8686013b7c9959938fddab6d7c1b95e4f02189
Philasande Bhani <pbhanina@gmail.com>
2023-06-23 14:17:01 +0200

    Menu button to be implemented

--- React Native. Job finder/components/common/header/ScreenHeaderBtn.jsx
+++ React Native. Job finder/components/common/header/ScreenHeaderBtn.jsx
@@ -2,12 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { Image, TouchableOpacity, Modal, Text, View } from "react-native";
 import QRCode from 'react-native-qrcode-svg';
 import styles from "./screenheader.style";
-import { SafeAreaView } from 'react-native-safe-area-context';
+// import { SafeAreaView } from 'react-native-safe-area-context';
 
 const ScreenHeaderBtn = ({ iconUrl, dimension }) => {
   const [profileLink, setProfileLink] = useState("https://www.linkedin.com/in/mr-p-bhani/");
   const [isModalVisible, setModalVisible] = useState(false);
   const [showProfile, setShowProfile] = useState(false);
+  const[showMenu, setMenu] = useState(false);
 
   const toggleModal = () => {
     setModalVisible(!isModalVisible);
@@ -20,6 +21,8 @@ const ScreenHeaderBtn = ({ iconUrl, dimension }) => {
   useEffect(() => {
     if(dimension === "100%")
     setShowProfile(true);
+    if(dimension === "60%")
+    setMenu(true);
   }, []);
 
   return (
@@ -38,6 +41,19 @@ const ScreenHeaderBtn = ({ iconUrl, dimension }) => {
             <View style={styles.modalContent}>
               <Text style={styles.headerText}>Scan this QR-Code</Text>
               <QRCode value={profileLink} size={200} />
+              <TouchableOpacity onPress={setToFalse} style={styles.btnContainer}>
+                <Text>Back</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
+      )}
+      {showMenu && (
+        <Modal visible={isModalVisible} animationType="slide" transparent>
+          <View style={styles.modalContainer}>
+            <View style={styles.modalContent}>
+              <Text style={styles.headerText}>To be implemented</Text>
+              
               <TouchableOpacity onPress={setToFalse} style={styles.btnContainer}>
                 <Text>Back</Text>
               </TouchableOpacity>
